94fde78142d62d78a5e2101c2d6bb22845a62a01
viniciusGuterres <viniguter@gmail.com>
2022-03-15 19:25:03 -0300

    removing test python project

--- face-detector/banco.sql
+++
@@ -1,9 +0,0 @@
-drop database verifica;
-create database if not exists verifica;
-use verifica;
-drop table if exists verifica.epidados;
-create table if not exists verifica.epidados(
-    datahora varchar(80),
-    predicao varchar(128)
-);
-select * from verifica.epidados;
